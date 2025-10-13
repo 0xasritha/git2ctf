290bba1be907edb30c1d43c3b3824b757fe5812e
Asritha <asrithab2022@gmail.com>
2025-10-12 21:11:52 -0400

    update

--- infra/pr_checks.py
+++ infra/pr_checks.py
@@ -1,1 +1,3 @@
 # TODO: pass in arg, and then execute that specific function, so can break up visually in Github Actions 
+
+# TODO: make sure all variations of "docker-compose.yaml" are named "compose.yaml"
