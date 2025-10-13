9a457897ae7cfbc2368666d69152d71758e26059
Asritha <asrithab2022@gmail.com>
2025-10-13 02:12:25 -0400

    dafs

--- challenges/web/xss/app.py
+++ challenges/web/xss/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.get("/")
 def hello():
     return "XSS WEB\n"
-
+#lkajsdf
 if __name__ == "__main__":
     # Bind to all interfaces inside the container
     app.run(host="0.0.0.0", port=8000)
